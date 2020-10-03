37acef386f639fc1a58a448d2e025367259ad4c2
jesusnboone <jesusnboone@gmail.com>
2020-10-03 17:31:03 -0500

    added nav element

---
+++ src/components/Nav/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "../../index.css";
+
+function Nav(props) {
+    return (
+        <div class="col align-bottom">
+
+            <div class="grey">
+                <nav class="navbar justify-content-end">
+                    <a class="navbar-text" href="#portfolio">
+                        Portfolio
+            </a>
+                    <a class="navbar-text" href="./assets/pdf/resume_jnb.pdf" target="_blank">
+                        Resume
+            </a>
+                    <a class="navbar-text" href="https://mail.google.com/mail/?view=cm&fs=1&to=jesusnboone@gmail.com" target="_blank">
+                        Contact
+            </a>
+                </nav>
+
+            </div>
+        </div>
+
+    );
+}
+
+export default Nav;
