b59b215dc4604838fc8039f938ba5391d5404882
jesusnboone <jesusnboone@gmail.com>
2020-10-04 13:20:58 -0500

    Updates

---
+++ precache-manifest.cc31e0a0ee84bf3ec6ad7f8cf5ddfa9b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a2d3fe316ed5ec4041068e224fcfb6",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a676e195b4205bf41bd4",
+    "url": "/react-portfolio/static/css/main.a79729d3.chunk.css"
+  },
+  {
+    "revision": "256dfe670d6fbf475a77",
+    "url": "/react-portfolio/static/js/2.0441e732.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.0441e732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a676e195b4205bf41bd4",
+    "url": "/react-portfolio/static/js/main.7035b140.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "a692421136a22d839ea880ca8e953c10",
+    "url": "/react-portfolio/static/media/headshot.a6924211.png"
+  },
+  {
+    "revision": "27f37cee2ab817ef72af920b562dac82",
+    "url": "/react-portfolio/static/media/note-taker.27f37cee.png"
+  },
+  {
+    "revision": "f72adbdd56b93b828e784ef00d58c70f",
+    "url": "/react-portfolio/static/media/password-generator.f72adbdd.png"
+  },
+  {
+    "revision": "17b29196a5fb31e39d52838ea8b0a09d",
+    "url": "/react-portfolio/static/media/red_bag.17b29196.png"
+  },
+  {
+    "revision": "b2502412d781bdcad6cff13fc135860a",
+    "url": "/react-portfolio/static/media/resume_jnb.b2502412.jpg"
+  },
+  {
+    "revision": "ae06c62896d36cd3512732efe9ba574e",
+    "url": "/react-portfolio/static/media/work-scheduler.ae06c628.png"
+  },
+  {
+    "revision": "3106e60409988acea5dc948914596d48",
+    "url": "/react-portfolio/static/media/worth-my-time.3106e604.png"
+  }
+]);
